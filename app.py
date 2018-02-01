2e5281d92d1dcda95a612a48f79e17b2ca4ce330
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-01-31 16:47:17 -0800

    Draft

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
+import json
 from binance.client import Client
 from config import Config
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(Config)
+client = Client(app.config['API_KEY'], app.config['API_SECRET'])
 
 
-@app.route('/')
-def index():
-    client = Client(app.config['API_KEY'], app.config['API_SECRET'])
+def get_info():
     info = client.get_account()
 
     for balance in info['balances']:
         total_balance = float(balance['free']) + float(balance['locked'])
         if total_balance > 0:
             print('{}: {}'.format(balance['asset'], total_balance))
+
+
+def get_all_orders():
+    orders = client.get_all_orders(symbol='ICXBTC')
+    print(json.dumps(orders, indent=4, sort_keys=True))
+
+
+@app.route('/')
+def index():
+    get_info()
+    get_all_orders()
 
     return ''
 
